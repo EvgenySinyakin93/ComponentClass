1648286ce3404bf389e6b1518a98356859934de7
EvgenySinyakin93 <evgeny.siniakin83@gmail.com>
2024-08-14 18:17:00 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css';
-import ShopItemClass from './components/ShopItemClass/ShopItemClass';
+import ShopItemClass from './components/shopItemFunc/components/shopItemFunc/ShopItemClass/ShopItemClass';
 
 function App() {
 
